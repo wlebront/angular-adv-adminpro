51c3eab5be3609e6ad9bb37c1eaac9e66de0680f
William Lebron <WilliamLebron23@hotmail.com>
2021-03-04 01:18:11 -0400

    Fin seccion 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,16 +14,19 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
     NopagefoundComponent,
+ 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PagesModule,
     AuthModule,
+    
   ],
   bootstrap: [AppComponent]
 })
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-import { RouterModule} from '@angular/router';
+
+
+//Modulos
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
+
+
+
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
+
 
 
 
@@ -24,13 +33,16 @@ import { SharedModule } from '../shared/shared.module';
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    
 
   ],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule,
-   
+    FormsModule,
+    ComponentsModule,
+ 
 
   ]
 })
